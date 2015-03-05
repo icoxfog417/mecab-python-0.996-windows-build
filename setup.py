94fbe77fb85a5b718945f895b04ff04b5cca8142
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-03-05 14:44:58 +0900

    compiled on windows7 32bit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup,Extension,os
+import string
+
+def cmd1(str):
+    return os.popen(str).readlines()[0][:-1]
+
+def cmd2(str):
+    return string.split (cmd1(str))
+
+setup(name = "mecab-python",
+	version = "0.996",
+	py_modules=["MeCab"],
+	ext_modules = [
+		Extension("_MeCab",
+			["MeCab_wrap.cxx",],
+			include_dirs=["C:/Users/tie301837/Documents/environments/MeCab/sdk"],
+			library_dirs=["C:/Users/tie301837/Documents/environments/MeCab/sdk"],
+			libraries=["libmecab"])
+			])
